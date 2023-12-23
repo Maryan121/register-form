bfaae2a3ac5e6afaa0054dc83fe81c7325902b0d
Maryan121 <maryanmaxamuudsabriye@gmail.com>
2023-12-23 12:22:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const[validEmail,setValidEmail] = useState(true)
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: ''
+  });
+  const [errormessage, setErrorMessage] = useState();
+
+  function handleChange(event){
+    setFormData(
+      {...formData,
+        [event.target.name]: event.target.value}
+    )
+    
+  }
+
+  function validateEmail(){
+    const emailRegex = /^[^\s@]+@[\s@]+\.[\s@]+$/;
+    const isValidEmail = emailRegex.test(formData.email);
+    setValidEmail(isValidEmail)
+    console.log(validEmail);
+     
+  }
+ 
+  
+  function handleSubmit(e){
+    e.preventDefault();
+    validateEmail();
+    console.log(
+      `First Name: ${formData.firstName}, Last Name: ${formData.lastName}, Email: ${formData.email}, Message: ${formData.message}`
+    )
+    
+    setErrorMessage(!validEmail? 
+      <span id='errorMessage'>enter a valid email</span> 
+      :<span></span>);
+
+    if(validEmail){
+      formData.firstName = ''
+      formData.lastName = ''
+      formData.email = ''
+      formData.message = ''
+      
+    }
+    
+   
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>contact us</h1>
+      <form className='form' onSubmit={handleSubmit}>
+        <input name="firstName" placeholder='first-name' onChange={handleChange} value={formData.firstName} required/>
+        <input name="lastName" placeholder='last-name' onChange={handleChange} value={formData.lastName} required/>
+        <div className='emailSection'>
+            <input name="email" id={!validEmail? 'errorBorder': ''} placeholder='email' onChange={handleChange} value={formData.email}  required/>           
+         {errormessage}
+        </div>
+        <textarea name='message' placeholder='message' onChange={handleChange} value={formData.message}></textarea>
+        <button>submit</button>
+      </form>
     </div>
   );
 }
-
-export default App;
